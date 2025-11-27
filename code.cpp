765174b6672d0ddd17a797b01e53a3d7d4934931
as3 <asertes2007@mail.ru>
2025-11-27 14:13:10 +0300

    changed min element

--- code.cpp
+++ code.cpp
@@ -14,14 +14,14 @@ int main() {
         cin >> arr[i];
     }
 
-    int max = arr[0];
+    int min = arr[0];
     for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+        if (arr[i] < min) {
+            min = arr[i];
         }
     }
 
-    cout << "Max: " << max << endl;
+    cout << "Min: " << min << endl;
 
 
     return 0;
